b6c63891f8de0b4353d9660bc2ad6043541b561f
Anderson Leal <andersonpains@yahoo.com.br>
2020-06-09 18:54:17 -0300

    Criação da Classe C.

---
+++ C.java
@@ -0,0 +1,25 @@
+public class C{
+    private String C1;
+    private int C2;
+
+    public String getC1() {
+        return C1;
+    }
+    public int getC2() {
+        return C2;
+    }
+    public void setC1(final String C1) {
+        this.C1 = C1;
+    }
+
+    public void setC2(final int C2) {
+        this.C2 = C2;
+    }
+
+    public String MC1(){
+        return "Método C1";
+    }
+    public String MC2(){
+        return "Método C2";
+    }
+}
