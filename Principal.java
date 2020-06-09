5ce966a97b1f2925bf62264f99b0ffb948c3b519
Anderson Leal <andersonpains@yahoo.com.br>
2020-06-09 19:03:43 -0300

    Atualizado a Classe Principal de forma a imprimir os métodos MA3(), MC1() e MC2().

--- Principal.java
+++ Principal.java
@@ -2,10 +2,14 @@ public class Principal {
     public static void main(String[] args) {
         A a = new A();
         B b = new B();
+        C c = new C();
         
         System.out.println(a.MA1());
         System.out.println(a.MA2());
         System.out.println(b.MB1());
         System.out.println(b.MB2());
+        a.MA3();
+        System.out.println(c.MC1());
+        System.out.println(c.MC2());
     }
 }
