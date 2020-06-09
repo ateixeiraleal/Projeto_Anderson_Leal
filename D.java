01715098fc3836c3432fed3dbe5f2d881c96be31
Anderson Leal <andersonpains@yahoo.com.br>
2020-06-09 20:45:49 -0300

    Criada a Classe D com seus respectivos métodos.

---
+++ D.java
@@ -0,0 +1,25 @@
+public class D{
+    private String D1;
+    private double D2;
+
+    public String getD1() {
+        return D1;
+    }
+    public double getD2() {
+        return D2;
+    }
+
+    public void setD1(String D1){
+        this.D1 = D1;
+    }
+    public void setD2(double D2){
+        this.D2 = D2;
+    }
+
+    public void MD1(){
+        System.out.println("Método D1");
+    }
+    public void MD2(){
+        System.out.println("Método D2");
+    }
+}
